fd8ff6ebddf389c34f3bdc2668c25262554a1353
efekaanguler <efekaanguler05@gmail.com>
2025-03-03 21:14:58 +0300

    add yolo_training

---
+++ UAV Detection/yolo/yolo_train.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+from tqdm import tqdm
+
+model = YOLO("yolo11n.pt") 
+
+data_yaml = "dataset.yaml"  
+
+epochs = 5
+model.train(
+    data=data_yaml,      # Dataset yaml dosyası (train/val klasör yollarını içermeli)
+    epochs=epochs,       # Eğitim epoch sayısı
+    batch=16,            # Batch size
+    imgsz=640,           # Görüntü boyutu
+    device="cuda",       # GPU kullanımı
+    )
+
+print("Done.")
