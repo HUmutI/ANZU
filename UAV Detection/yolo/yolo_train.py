8af773f020eef0bffe92dc923a352e181b521e03
efekaanguler <efekaanguler05@gmail.com>
2025-03-08 00:32:30 +0300

    add multiple yolo train script

--- UAV Detection/yolo/yolo_train.py
+++ UAV Detection/yolo/yolo_train.py
@@ -1,17 +1,28 @@
 from ultralytics import YOLO
-from tqdm import tqdm
 
-model = YOLO("yolo11n.pt") 
+models = ["yolo11n.pt", "yolo11s.pt", "yolo11m.pt", "yolo11l.pt", "yolo11x.pt"]
 
-data_yaml = "dataset.yaml"  
+data_yaml = "dataset.yaml"
 
-epochs = 50
-model.train(
-    data=data_yaml,      # Dataset yaml dosyası (train/val klasör yollarını içermeli)
-    epochs=epochs,       # Eğitim epoch sayısı
-    batch=16,            # Batch size
-    imgsz=640,           # Görüntü boyutu
-    device="cuda",       # GPU kullanımı
+epochs = 120
+batch_size = 32
+img_size = 640
+device = "cuda"
+
+for model_name in models:
+    print(f"\n{model_name} modeli eğitiliyor...")
+    
+    model = YOLO(model_name)    
+    model.train(
+        data=data_yaml,
+        epochs=epochs,
+        batch=batch_size,
+        imgsz=img_size,
+        device=device,
+        project="yolo11_models", 
+        name=model_name.split(".")[0],
     )
+    
+    print(f"{model_name} modeli eğitimi tamamlandı.\n")
 
-print("Done.")
+print("Tüm modellerin eğitimi tamamlandı.")
